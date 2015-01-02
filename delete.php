c95b9d30769bff4d271eedfca68e5fb9c8080cbd
Chao Liu <liuchao200240@gmail.com>
2015-01-02 09:22:33 -0800

    hw4

--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php if(isset($_GET['id'])):?>
-<?php require_once('lib/news_model.php');
-	$news=new News_model();
-	$news->delete_news($_GET['id']);
-?>
-<?php endif;?>
-<?php header('Location: index.php');?>
